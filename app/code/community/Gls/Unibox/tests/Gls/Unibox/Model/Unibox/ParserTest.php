cec293d12b4af01ab969212bc6532e443e5c3f9b
Andreas Mautz <am@webvisum.de>
2015-08-12 16:21:18 +0200

    [INIT] add first UnitTest

---
+++ app/code/community/Gls/Unibox/tests/Gls/Unibox/Model/Unibox/ParserTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class Gls_Unibox_Model_Unibox_ParserTest extends PHPUnit_Framework_TestCase {
+
+    public function testAddCheckDigit()
+    {
+        $number = 1234;
+        $_digitArray = str_split($number);
+        $_digitArray = array_reverse($_digitArray);
+        $sum = 0;
+        foreach($_digitArray as $key => $value){
+            if($key%2 == 0){
+                $sum += 3*$value;
+            }else{
+                $sum += $value;
+            }
+        }
+        $diff = (int)(ceil($sum/10)*10)-($sum+1);
+        if($diff == 10){
+            $returnVal = 0;
+        }else{
+            $returnVal = $number.$diff;
+        }
+
+        $this->assertEquals(
+            $diff,
+            7
+        );
+    }
+
+}
