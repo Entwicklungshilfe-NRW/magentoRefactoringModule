8017eb399e6525ecace8f85c3eebf20a13d905ce
Andreas Mautz <am@webvisum.de>
2015-08-13 18:57:36 +0200

    [INFRASTRUCTURE] small changes for php version and unittesting

--- app/code/community/Gls/Unibox/Test/Model/Unibox/ParserTest.php
+++ app/code/community/Gls/Unibox/Test/Model/Unibox/ParserTest.php
@@ -28,4 +28,30 @@ class Gls_Unibox_Test_Model_Unibox_ParserTest extends EcomDev_PHPUnit_Test_Case 
         );
     }
 
+    public function testFailAddCheckDigit()
+    {
+        $number = 1235;
+        $_digitArray = str_split($number);
+        $_digitArray = array_reverse($_digitArray);
+        $sum = 0;
+        foreach($_digitArray as $key => $value){
+            if($key%2 == 0){
+                $sum += 3*$value;
+            }else{
+                $sum += $value;
+            }
+        }
+        $diff = (int)(ceil($sum/10)*10)-($sum+1);
+        if($diff == 10){
+            $returnVal = 0;
+        }else{
+            $returnVal = $number.$diff;
+        }
+
+        $this->assertEquals(
+            $diff,
+            7
+        );
+    }
+
 }
